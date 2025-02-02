9c360aee0cc49279f6217523f41d9c6035c3aab5
Javier García <javier.garcia.olmeda@gmail.com>
2025-02-02 12:57:55 +0100

    feat: 404 when booking id not exists

--- src/main/java/com/ademlo/bookinghotel/features/booking/infra/rest/BookingController.java
+++ src/main/java/com/ademlo/bookinghotel/features/booking/infra/rest/BookingController.java
@@ -2,11 +2,10 @@ package com.ademlo.bookinghotel.features.booking.infra.rest;
 
 import com.ademlo.bookinghotel.features.booking.domain.model.Booking;
 import com.ademlo.bookinghotel.features.booking.infra.persistence.BookingLocalRepository;
-import com.ademlo.bookinghotel.features.booking.infra.rest.DTO.BookingResponse;
 import com.ademlo.bookinghotel.features.booking.infra.rest.DTO.BookingDTO;
+import com.ademlo.bookinghotel.features.booking.infra.rest.DTO.BookingResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.Date;
 
 @RestController
 @RequestMapping("/booking")
@@ -19,15 +18,18 @@ public class BookingController {
     }
 
     @PostMapping("")
-    public BookingResponse createBooking(@RequestBody BookingDTO bookingDTO){
+    public BookingResponse createBooking(@RequestBody BookingDTO bookingDTO) {
         String bookingId = bookingLocalRepository.addBooking(
-                Booking.newBooking(bookingDTO.getEmployeeId(), bookingDTO.getRoomId(),bookingDTO.getStartDate(), bookingDTO.getEndDate()));
+                Booking.newBooking(bookingDTO.getEmployeeId(), bookingDTO.getRoomId(), bookingDTO.getStartDate(), bookingDTO.getEndDate()));
         return new BookingResponse(bookingId, "Reservation confirmed");
     }
 
     @GetMapping("/{bookingId}")
-    public BookingDTO createBooking(@PathVariable String bookingId){
+    public ResponseEntity<BookingDTO> createBooking(@PathVariable String bookingId) {
         Booking booking = bookingLocalRepository.getBookingBy(bookingId);
-        return new BookingDTO(booking.getEmployeeId(), booking.getRoomId(), booking.getStartDate(),booking.getEndDate());
+        if (booking == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new BookingDTO(booking.getEmployeeId(), booking.getRoomId(), booking.getStartDate(), booking.getEndDate()));
     }
 }
